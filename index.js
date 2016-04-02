7fae8fc678558cb406504849e33e42eb1bea1df8
mdrummond <mdrummond@mac.com>
2016-04-01 19:59:07 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * gulp-sass-globbing
+ * https://github.com/mdrummond/gulp-sass-globbing
+ *
+ * Modeled after grunt-sass-globbing: takes in actual files and outputs a new
+ * file containing a list of @import rules in sass. Does not use regex to parse
+ * an @import statement within the incoming file stream.
+ *
+ * Copyright (c) 2016 Marc Drummond
+ * Licensed under the MIT license.
+ */
+
+var slash = require('slash'),
+   path = require('path');
+   fs = require('fs');
+   through = require('through2');
+   glob = require('glob');
+
+module.exports = function() {
+  var transform = function(file, encoding, callback) {
+    // File source required.
+    if (file.isNull()) {
+      // nothing to do
+      return callback(null, file);
+    }
+
+    // Complete files required, so streams are not supported.
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported.'));
+    }
+    else if (file.isBuffer()) {
+
+      return callback(null, file);
+    }
+  };
+
+  return through.obj(transform);
+};
